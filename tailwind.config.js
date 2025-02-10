a9cac4a8cf32a52906082f42104a781bc2b1de31
pyae-phyoe-maung <p666766@gmail.com>
2025-02-11 01:24:21 +0630

    Add project files.

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './Pages/**/*.cshtml',
+        './Views/**/*.cshtml',
+        './Views/Shared/**/*.cshtml'
+    ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
